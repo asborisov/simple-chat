ae515c2fee1df102fdeb87fa3a2b169854cd1939
asborisov <timan.b@gmail.com>
2018-02-13 08:31:07 +0700

    Elm formatting

--- client/elm/perser2.elm
+++ client/elm/perser2.elm
@@ -4,7 +4,7 @@
 module Main exposing (..)
 
 import Date exposing (Date, fromString)
-import Html exposing (br, div, text, Html)
+import Html exposing (Html, br, div, text)
 import Json.Decode exposing (Decoder)
 import Result exposing (andThen)
 
@@ -15,6 +15,7 @@ import Result exposing (andThen)
 --    , uid : String
 --    , text : String
 --    }
+
 
 type ResponseType
     = LOGIN
@@ -28,73 +29,95 @@ type Error
 
 
 type alias Message =
-  {
-      timeStamp: Date,
-      text: String
-  }
+    { timeStamp : Date
+    , text : String
+    }
+
 
 type alias Login =
-  {
-    user: String,
-    text: String
-  }
+    { user : String
+    , text : String
+    }
 
-type ReturnResult =
-    MessageRes Message |
-    LoginRes Login |
-    Error String
+
+type ReturnResult
+    = MessageRes Message
+    | LoginRes Login
+    | Error String
+
 
 timestampToDate : Decoder Date
 timestampToDate =
     Json.Decode.float
         |> Json.Decode.andThen (\val -> Json.Decode.succeed (Date.fromTime val))
 
+
 messageDecoder : Decoder ReturnResult
 messageDecoder =
-  Json.Decode.map2 Message
-    (Json.Decode.field "ts" timestampToDate)
-    (Json.Decode.field "text" Json.Decode.string)
-  |> Json.Decode.andThen (\val -> Json.Decode.succeed (MessageRes val))
+    Json.Decode.map2 Message
+        (Json.Decode.field "ts" timestampToDate)
+        (Json.Decode.field "text" Json.Decode.string)
+        |> Json.Decode.andThen (\val -> Json.Decode.succeed (MessageRes val))
+
 
 loginDecoder : Decoder ReturnResult
 loginDecoder =
-  Json.Decode.map2 Login
-    (Json.Decode.field "user" Json.Decode.string)
-    (Json.Decode.field "text" Json.Decode.string)
-  |> Json.Decode.andThen (\val -> Json.Decode.succeed (LoginRes val))
+    Json.Decode.map2 Login
+        (Json.Decode.field "user" Json.Decode.string)
+        (Json.Decode.field "text" Json.Decode.string)
+        |> Json.Decode.andThen (\val -> Json.Decode.succeed (LoginRes val))
+
 
 decoder : String -> Decoder ReturnResult
 decoder type_ =
-  case type_ of
-    "message" -> messageDecoder
-    "login" -> loginDecoder
-    _ -> Json.Decode.fail "Unknown type"
+    case type_ of
+        "message" ->
+            messageDecoder
+
+        "login" ->
+            loginDecoder
+
+        _ ->
+            Json.Decode.fail "Unknown type"
+
 
 parseMessage : String -> ReturnResult
 parseMessage input =
-  let
-    messageType = Result.withDefault "" (Json.Decode.decodeString (Json.Decode.field "type" Json.Decode.string) input)
-  in
-      input
-      |> Json.Decode.decodeString (decoder messageType)
-      |> (\res ->
-        case res of
-          Ok okRes -> okRes
-          Err err -> Error err)
+    let
+        messageType =
+            Result.withDefault "" (Json.Decode.decodeString (Json.Decode.field "type" Json.Decode.string) input)
+    in
+    input
+        |> Json.Decode.decodeString (decoder messageType)
+        |> (\res ->
+                case res of
+                    Ok okRes ->
+                        okRes
+
+                    Err err ->
+                        Error err
+           )
+
 
 resultToString : ReturnResult -> String
 resultToString result =
-  case result of
-    MessageRes msg -> "ts: " ++ (toString msg.timeStamp) ++ "text: " ++ msg.text
-    LoginRes login -> "User " ++ login.user ++ " logged in: " ++ login.text
-    Error err -> err
+    case result of
+        MessageRes msg ->
+            "ts: " ++ toString msg.timeStamp ++ "text: " ++ msg.text
+
+        LoginRes login ->
+            "User " ++ login.user ++ " logged in: " ++ login.text
+
+        Error err ->
+            err
+
 
 main : Html msg
 main =
-    div [] [
-      text (resultToString (parseMessage "{\"type\": \"message\",  \"ts\": \"1518165045444\", \"text\": \"Hello!\"}")),
-      br [][],
-      text (resultToString (parseMessage "{\"type\": \"message\", \"ts\": 1518165045444, \"text\": \"Hello!\"}")),
-      br [][],
-      text (resultToString (parseMessage "{\"type\": \"login\", \"ts\": 1518165045444, \"text\": \"Hello!\", \"user\": \"timan\"}"))
-    ]
+    div []
+        [ text (resultToString (parseMessage "{\"type\": \"message\",  \"ts\": \"1518165045444\", \"text\": \"Hello!\"}"))
+        , br [] []
+        , text (resultToString (parseMessage "{\"type\": \"message\", \"ts\": 1518165045444, \"text\": \"Hello!\"}"))
+        , br [] []
+        , text (resultToString (parseMessage "{\"type\": \"login\", \"ts\": 1518165045444, \"text\": \"Hello!\", \"user\": \"timan\"}"))
+        ]
